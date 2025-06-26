fea58698fffee0847fed3a713b3cd2e9e31dd80c
Gleb Shuhleev <glsergvch@gmail.com>
2025-06-27 02:44:53 +0500

    BKG-7 added delete route in backend/routes/api/room.php.

--- backend/routes/api/room.php
+++ backend/routes/api/room.php
@@ -6,6 +6,7 @@ use App\Http\Api\V1\Hotel\IndexHotelController;
 use App\Http\Api\V1\Hotel\ShowHotelController;
 use App\Http\Api\V1\Hotel\UpdateHotelController;
 use App\Http\Api\V1\Room\CreateRoomController;
+use App\Http\Api\V1\Room\DeleteRoomController;
 use App\Http\Api\V1\Room\IndexRoomController;
 use App\Http\Api\V1\Room\ShowRoomController;
 use App\Http\Api\V1\Room\UpdateRoomController;
@@ -18,6 +19,7 @@ Route::middleware(['auth:sanctum'])
         Route::get('/', IndexRoomController::class)->name('room.index');
         Route::get('/{id}', ShowRoomController::class)->name('room.show')->where('id', '[0-9]+');
         Route::put('/{id}', UpdateRoomController::class)->name('room.update')->where('id', '[0-9]+');
+        Route::delete('/{id}', DeleteRoomController::class)->name('room.delete')->where('id', '[0-9]+');
 //        Route::get('/{id}', ShowHotelController::class)->name('hotel.show')->where('id', '[0-9]+');
 //        Route::put('/{id}', UpdateHotelController::class)->name('hotel.update')->where('id', '[0-9]+');
 //        Route::delete('/{id}', DeleteHotelController::class)->name('hotel.delete')->where('id', '[0-9]+');
